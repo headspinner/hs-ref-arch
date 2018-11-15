3d9676cd96faa43969bc7e601671e05a4c5c4adb
Tori <tori@envoyai.com>
2018-11-15 13:29:51 -0500

    cleanup

--- HeadSpin.Reference/HeadSpin.Reference.Web2/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace HeadSpin.Reference.Web2.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- HeadSpin.Reference/HeadSpin.Reference.Web2/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HeadSpin.Reference.Web2.MvcApplication" Language="C#" %>
